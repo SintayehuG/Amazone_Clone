8bc67d6d494fe5ee1e549d24efb3bd9643e32c53
SintayehuG <sintayehu.getahun@gmail.com>
2025-10-16 21:45:59 -0500

    deploying  on netlify

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-    baseURL:"http://127.0.0.1:5001/ammazon-clone-21523/us-central1/api",
+    // baseURL:"http://127.0.0.1:5001/ammazon-clone-21523/us-central1/api",
+    baseURL: "https://us-central1-ammazon-clone-21523.cloudfunctions.net/api"
+    
 })
 export {axiosInstance};
