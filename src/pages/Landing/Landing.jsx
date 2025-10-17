ca2b998145c4b0f328eb3c75a54652ce8984a1dc
SintayehuG <sintayehu.getahun@gmail.com>
2025-10-16 23:20:31 -0500

    updated

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Layout from "../../Components/Layout/Layout";
+import LayOut from "../../Components/LayOut/LayOut"
 import Carousel from "../../Components/Carousel/CarouselEffect";
 import Category from "../../Components/Category/Category";
 import Product from "../../Components/Product/Product";
 
 function Landing() {
   return (
-    <Layout>
+    <LayOut>
       <Carousel />
       <Category />
       <Product />
-    </Layout>
+    </LayOut>
   );
 }
 
